0092e61a0871b833d2610de74e741c53068861e4
jgfruitbomb <jgfruitbomb@gmail.com>
2023-12-30 20:50:47 -0600

    Add relay pinout

--- websocket_arduino/websocket_arduino.ino
+++ websocket_arduino/websocket_arduino.ino
@@ -1,12 +1,18 @@
 #include <WiFi.h>
 #include <WebSocketsServer.h>
+#include <HardwareSerial.h>
 
 #define INTERNAL_LED 2
+
+HardwareSerial Ultrasonic_Sensor(2); //   TX2 (pin 17), RX2 (pin 16) 
+ 
+unsigned char data[4] = {};
+float distance;
 
 const char* ssid = "EatAssDownloadFast";
 const char* password = "AmericasA$$";
 const char *webSocketUrl = "ws://192.168.1.185";
-uint8_t count = 0;
+uint8_t count = 10;
 
 WebSocketsServer webSocket = WebSocketsServer(80);
 
@@ -27,10 +33,11 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
 
     if ( strncmp((char*)payload, "zap", 3) == 0)
     {
-      Serial.println("TURNING ON LIGHT");
-      digitalWrite(INTERNAL_LED, HIGH);
-      delay(5000);
-      digitalWrite(INTERNAL_LED, LOW);
+      digitalWrite(18, HIGH);
+  
+      delay(1000);  // 1000 milliseconds or 1 second
+  
+      digitalWrite(18, LOW);
     }
 
     break;
@@ -47,7 +54,7 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
 }
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   delay(1000);
   pinMode(INTERNAL_LED, OUTPUT);
 
@@ -68,21 +75,49 @@ void setup() {
 
   webSocket.begin();
   webSocket.onEvent(webSocketEvent);
+  Ultrasonic_Sensor.begin(9600); // Initialize the hardware serial
 
+  // Control relay
+  pinMode(18, OUTPUT);
+  digitalWrite(18, LOW);
 }
 
 void loop() {    
   webSocket.loop();
 
-  if (0 == count)
-  {
-    count = 150;
-  }
-  else {
-   count = 0;
-  }
-  String counter = String(count);
-  webSocket.broadcastTXT(counter);
-  //count++;
-  //delay(1000);
+  do{
+     for(int i=0;i<4;i++)
+     {
+       data[i]=Ultrasonic_Sensor.read();
+     }
+  }while(Ultrasonic_Sensor.read()==0xff);
+
+  Ultrasonic_Sensor.flush();
+
+  if(data[0]==0xff)
+    {
+      int sum;
+      sum=(data[0]+data[1]+data[2])&0x00FF;
+      if(sum==data[3])
+      {
+        distance=(data[1]<<8)+data[2];
+        if(distance>30)
+          {
+           Serial.print("distance=");
+           Serial.print(distance/10);
+           Serial.println("cm");
+                 //Serial.print("distance=");
+      //distance=distance / 10;
+      //Serial.println(distance);
+
+      //String myString;
+      //myString = String(distance);
+      //webSocket.broadcastTXT(myString);
+          }else 
+             {
+               Serial.println("Below the lower limit");
+             }
+      }else Serial.println("ERROR");
+     }
+     delay(100);
 }
